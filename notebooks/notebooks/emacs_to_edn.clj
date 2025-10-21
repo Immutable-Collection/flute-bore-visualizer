d88805235ad3f63d577174bb8056f8b3d47a13da
Iain Wood <iain@soulflyer.co.uk>
2025-10-21 16:58:47 +0700

    Working on org-edn
    Co-authored-by: siavash mohammady <siavashmohammady66@users.noreply.github.com>

--- notebooks/notebooks/emacs_to_edn.clj
+++ notebooks/notebooks/emacs_to_edn.clj
@@ -13,19 +13,46 @@
 
 (def lot-dcm615-content (slurp lot-dcm615-file-path))
 
+(defn valid-prefix? [s]
+  (boolean (re-find #"^(\|+ |\*+ )" s)))
+
 (def initially-cleaned-content
   (filter
-    ;; TODO for finding heading use a regex with pattern *** followed by a space
-
-    #(or
-       (str/starts-with? % "|")
-       (str/starts-with? % "*"))
-
+    valid-prefix?
     (str/split-lines lot-dcm615-content)))
 
 initially-cleaned-content
 
+(defn count-* [s](get (frequencies s) \* 0))
+
+(defn is-table-header? [s]
+  (boolean (re-find #"^\|\s*([A-Za-z]+)" s)))
+
+(defn is-table-content? [s]
+  (boolean (re-find #"^\|\s*(\d+(?:\.\d+)?)" s)))
+
+(defn is-org-header? [s]
+  (str/starts-with? s "*"))
+
+(defn get-row-items [s] 
+(map str/trim (rest (str/split s #"\|"))))
+
+(defn process-headings [])
+(defn process-table-header [s]
+(map str/lower-case (get-row-items s)))
+(defn process-table-row [s]
+(map Float/parseFloat (get-row-items s)))
+
 ;; # processing contents
-#_(map
- #()
- initially-cleaned-content)
+(def processed-content (map-indexed
+ #(cond 
+    (is-table-content? %2) {:type :table-content :content %2 :row %1 :data (process-table-row %2)}
+    (is-table-header?  %2) {:type :table-heading :content %2 :row %1 :data (process-table-header %2)} 
+    (is-org-header?    %2) {:type :heading :level (count-* %2) :content %2 :row %1}
+    :else nil)
+ initially-cleaned-content))
+
+processed-content
+
+
+(filter #(= :table-heading (:type %)) processed-content)
