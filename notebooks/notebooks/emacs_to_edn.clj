758f4f4619685a2d917d58f1187eefc6427ef07a
Iain Wood <iain@soulflyer.co.uk>
2025-10-27 16:56:03 +0700

    Working on org-edn
    Co-authored-by: siavash mohammady <siavashmohammady66@users.noreply.github.com>

--- notebooks/notebooks/emacs_to_edn.clj
+++ notebooks/notebooks/emacs_to_edn.clj
@@ -13,7 +13,8 @@
 
 (def lot-dcm615-content (slurp lot-dcm615-file-path))
 
-(defn valid-prefix? [s]
+(defn valid-prefix?
+  [s]
   (boolean (re-find #"^(\|+ |\*+ )" s)))
 
 (def initially-cleaned-content
@@ -23,36 +24,99 @@
 
 initially-cleaned-content
 
-(defn count-* [s](get (frequencies s) \* 0))
+(defn count-*
+  [s] (get (frequencies s) \* 0))
 
-(defn is-table-header? [s]
+(defn is-table-header?
+  [s]
   (boolean (re-find #"^\|\s*([A-Za-z]+)" s)))
 
-(defn is-table-content? [s]
+(defn is-table-content?
+  [s]
   (boolean (re-find #"^\|\s*(\d+(?:\.\d+)?)" s)))
 
-(defn is-org-header? [s]
+(defn is-org-header?
+  [s]
   (str/starts-with? s "*"))
 
-(defn get-row-items [s] 
-(map str/trim (rest (str/split s #"\|"))))
+(defn get-row-items
+  [s]
+  (map str/trim (rest (str/split s #"\|"))))
 
-(defn process-headings [])
-(defn process-table-header [s]
-(map str/lower-case (get-row-items s)))
-(defn process-table-row [s]
-(map Float/parseFloat (get-row-items s)))
+(defn process-headings
+  [])
+
+(defn convert-title-to-keyword [header]
+(-> header
+ str/lower-case
+ (str/replace " " "-")
+ keyword))
+
+(defn process-table-header
+  [s]
+  (map convert-title-to-keyword (get-row-items s)))
+
+(defn process-table-row
+  [s]
+  (map Float/parseFloat (get-row-items s)))
 
 ;; # processing contents
-(def processed-content (map-indexed
- #(cond 
-    (is-table-content? %2) {:type :table-content :content %2 :row %1 :data (process-table-row %2)}
-    (is-table-header?  %2) {:type :table-heading :content %2 :row %1 :data (process-table-header %2)} 
-    (is-org-header?    %2) {:type :heading :level (count-* %2) :content %2 :row %1}
-    :else nil)
- initially-cleaned-content))
+(def processed-content
+  (map-indexed
+   #(cond
+      (is-table-content? %2) {:type :table-content
+                              :content %2
+                              :row %1
+                              :data (process-table-row %2)}
+      (is-table-header?  %2) {:type :table-heading
+                              :content %2 :row %1
+                              :data (process-table-header %2)}
+      (is-org-header?    %2) {:type :heading
+                              :level (count-* %2)
+                              :content (convert-title-to-keyword
+                                        (-> %2
+                                            (str/replace "* " "")
+                                            (str/replace "*" "")))
+                              :row %1}
+      :else nil)
+   initially-cleaned-content))
 
 processed-content
 
+(filter #(= :heading (:type %)) processed-content)
 
-(filter #(= :table-heading (:type %)) processed-content)
+;; TODO table structure & table content
+
+;; ### creating map structure based on the heading levels
+
+(defn find-parent [input number]
+  (loop [input-list input
+         acc []]
+    (if ()
+      {}
+      (recur () ()))))
+
+
+(defn add-org-row-to-map [row result-map]
+  (if (= :heading (:type row))
+    (if (= 1 (:level row))
+      (assoc result-map 
+             (:content row) 
+             {:level (:level row) :number (:row row)})
+      ; should nest
+      (assoc-in result-map 
+                [(first (keys result-map)) (:content row)] 
+                {:level (:level row) :number (:row row)})
+      )
+
+    result-map))
+
+(defn org-processed-list-to-edn 
+   [input-list]
+   (loop [processing input-list
+          acc {}]
+     (if (=  0 (count processing))
+       acc
+       (recur (rest processing) (add-org-row-to-map (first processing) acc)))))
+
+(org-processed-list-to-edn processed-content)
