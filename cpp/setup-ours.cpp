8742855488db2da9b07322585b23f250b918b096
Iain <iain@soulflyer.co.uk>
2026-02-12 17:01:15 +0700

    Separted FluidX3d code
    Co-authored-by: siavash mohammady <siavashmohammady66@users.noreply.github.com>

---
+++ cpp/setup-ours.cpp
@@ -0,0 +1,52 @@
+#include "setup.hpp"
+
+void main_setup() { // aerodynamics of a cow; required extensions in defines.hpp: FP16S, EQUILIBRIUM_BOUNDARIES, SUBGRID, INTERACTIVE_GRAPHICS or GRAPHICS
+	// ################################################################## define simulation box size, viscosity and volume force ###################################################################
+	const uint3 lbm_N = resolution(float3(2.0f, 2.0f, 2.0f), 1000u); // input: simulation box aspect ratio and VRAM occupation in MB, output: grid resolution
+	const float si_u = 1.0f;
+	const float si_length = 2.4f;
+	const float si_T = 10.0f;
+	const float si_nu=1.48E-5f, si_rho=1.225f;
+	const float lbm_length = 0.65f*(float)lbm_N.y;
+	const float lbm_u = 0.75f;
+	units.set_m_kg_s(lbm_length, lbm_u, 1.0f, si_length, si_u, si_rho);
+	const float lbm_nu = units.nu(si_nu);
+	const ulong lbm_T = units.t(si_T);
+	print_info("Re = "+to_string(to_uint(units.si_Re(si_length, si_u, si_nu))));
+	print_info("U = "+to_string(si_u)+" m/s");
+	LBM lbm(lbm_N, lbm_nu);
+	// ###################################################################################### define geometry ######################################################################################
+	const float3x3 rotation = float3x3(float3(1, 0, 0), radians(180.0f))*float3x3(float3(0, 0, 1), radians(180.0f));
+	Mesh* mesh = read_stl(get_exe_path()+"../stl/com_lot.stl", lbm.size(), lbm.center(), rotation, lbm_length); // https://www.thingiverse.com/thing:182114/files
+	//mesh->translate(float3(0.0f, 1.0f-mesh->pmin.y+0.1f*lbm_length, 1.0f-mesh->pmin.z)); // move mesh forward a bit and to simulation box bottom, keep in mind 1 cell thick box boundaries
+	lbm.voxelize_mesh_on_device(mesh);
+	const uint Nx=lbm.get_Nx(), Ny=lbm.get_Ny(), Nz=lbm.get_Nz(); 
+	// print nx ny & nz
+	print_info("Nx = "+to_string(Nx));
+	print_info("Ny = "+to_string(Ny));
+	print_info("Nz = "+to_string(Nz));
+	parallel_for(lbm.get_N(), [&](ulong n) { 	
+		uint x=0u, y=0u, z=0u; 
+		lbm.coordinates(n, x, y, z);
+		//if(z==0u) lbm.flags[n] = TYPE_S; // solid floor
+		if(lbm.flags[n]!=TYPE_S) {
+			if (x == 117u & y == 118u & z == 180u){
+				lbm.u.z[n] = lbm_u;
+			}
+
+			lbm.flags[n] =  TYPE_F;
+		} // initialize y-velocity everywhere except in solid cells
+		//if(x==0u||x==Nx-1u||y==0u||y==Ny-1u||z==Nz-1u) lbm.flags[n] = TYPE_E; // all other simulation box boundaries are inflow/outflow
+	}); // ####################################################################### run simulation, export images and data ##########################################################################
+	lbm.graphics.visualization_modes = VIS_FLAG_SURFACE|VIS_Q_CRITERION;
+#if defined(GRAPHICS) && !defined(INTERACTIVE_GRAPHICS)
+	lbm.graphics.set_camera_centered(-40.0f, 20.0f, 78.0f, 1.25f);
+	lbm.run(0u, lbm_T); // initialize simulation
+	while(lbm.get_t()<=lbm_T) { // main simulation loop
+		if(lbm.graphics.next_frame(lbm_T, 10.0f)) lbm.graphics.write_frame();
+		lbm.run(1u, lbm_T);
+	}
+#else // GRAPHICS && !INTERACTIVE_GRAPHICS
+	lbm.run();
+#endif // GRAPHICS && !INTERACTIVE_GRAPHICS
+}
